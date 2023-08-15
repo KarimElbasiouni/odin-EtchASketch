3544dcedfb83093e752f2e382f9dde08cad80a54
KarimElbasiouni <elbasiounikarim@gmail.com>
2023-08-15 21:42:30 +0300

    Add grid dimension slider

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const container = document.querySelector('#gridContainer');
-
+let numDimension = 0;
 // 'createGrid' creates an n x n grid of empty divs
 function createGrid(n = 16){ 
+    numDimension = n;
     for(let i = 1; i<= n; i++){
         let subContainer = document.createElement('div')
         subContainer.setAttribute("id","subContainer" )
@@ -14,8 +15,14 @@ function createGrid(n = 16){
             subContainer.appendChild(newDiv);
         }
     }
+    container.backgroundColor = "white";
 }
 
+let color = '#000000';
+function pickColor(){
+    const colorPicker = document.querySelector('input[type = color]');
+    color = colorPicker.value;
+}
 
 let isMouseDown = false; 
 function sketch(){
@@ -31,17 +38,35 @@ function sketch(){
             e.target.style.backgroundColor = color;   
         }
     })
-
 }
 
-let color = '#000000'
-function pickColor(){
-    const colorPicker = document.querySelector('input[type = color]');
-    color = colorPicker.value;
+const gridSizeSlider = document.querySelector('input[type = range]');
+let gridSizeValue = 16;
+function adjustGridSize(){
+    const gridSizeSlider = document.querySelector('input[type = range]');
+    gridSizeValue = gridSizeSlider.value;
+    return gridSizeValue
+    
+}
+
+gridSizeSlider.addEventListener('change', (e)=>{
+    console.log(e);
+    let dimension = adjustGridSize();
+    document.querySelector('label#adjustSize').textContent = dimension + ' x ' + dimension;
+    clearGrid();
+    createGrid(dimension);
+})
+sketch();
+function clearGrid(){
+    let subContainers = document.querySelectorAll('div#subContainer');
+    subContainers.forEach((subContainer) => {
+        while (subContainer.firstChild){
+            subContainer.removeChild(subContainer.firstChild);
+        } 
+        subContainer.parentNode.removeChild(subContainer);
+    })
 
 
 }
 
 createGrid(16);
-sketch();
-pickColor();
